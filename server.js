e0f88f802cb824d10f7db67a57f83c1c8a47f23e
kidahosa <idahosakevinaibalegbagbon@gmail.com>
2026-05-22 22:00:46 +0100

    edited model to models

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
